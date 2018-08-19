8c4edcf3b1ad55c0a854c5e1b306d721282ebe86
Justin Wang <justinwang24@yahoo.com.au>
2018-08-19 13:26:56 +1000

    Update theme

--- healtree/templates/one_column.blade.php
+++ healtree/templates/one_column.blade.php
@@ -1,19 +1,21 @@
 @extends(_get_frontend_layout_path('frontend'))
 @section('content')
 <div class="container">
-        <div class="columns mt-20">
-                <div class="column is-1"></div>
-                <div class="column">
-                        <div class="content pt-10">
-                                <div class="page-title-wrap">
-                                        <h1 class="is-size-1-desktop is-size-1-mobile" style="margin-top:0px; padding-top: 20px;">
-                                                {!! 'cn'==app()->getLocale() ? $page->title_cn : $page->title !!}
-                                        </h1>
+        <div class="content">
+                <div class="columns mt-20">
+                        <div class="column is-1"></div>
+                        <div class="column">
+                                <div class="content pt-10">
+                                        <div class="page-title-wrap">
+                                                <h1 class="is-size-1-desktop is-size-1-mobile" style="margin-top:0px; padding-top: 20px;">
+                                                        {!! 'cn'==app()->getLocale() ? $page->title_cn : $page->title !!}
+                                                </h1>
+                                        </div>
+                                        {!! $page->rebuildContent() !!}
                                 </div>
-                                {!! $page->rebuildContent() !!}
                         </div>
+                        <div class="column is-1"></div>
                 </div>
-                <div class="column is-1"></div>
         </div>
 </div>
 @endsection
